8db371a659920f44bbb4fa61db8e3150e52c1123
frontender950 <lexabob.av@gmail.com>
2020-12-01 12:44:57 +0300

    fixed: slider html structure

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
+
   loop: true,
-  // Navigation arrows
+
   navigation: {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
@@ -13,10 +13,11 @@ var hotelSlider = new Swiper('.hotel-slider', {
     onlyInViewport: false,
   },
 })
+
 var rewiewsSlider = new Swiper('.rewiews-slider', {
-  // Optional parameters
+
   loop: true,
-  // Navigation arrows
+
   navigation: {
     nextEl: '.rewiews-slider__button--next',
     prevEl: '.rewiews-slider__button--prev',
